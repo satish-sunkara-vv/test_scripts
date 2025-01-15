57c3a3124e61a9c2571ef8bf2e9d23f7daa1e204
satish-sunkara-vv <satish.sunkara@nutanix.com>
2025-01-15 22:38:52 +0530

    commit-8

--- local_cloud_512k.py
+++ local_cloud_512k.py
@@ -10,6 +10,39 @@ import paramiko
 trunc_size=int(input("Please Enter the Truncated size"))
 bs=[1,int(trunc_size-(trunc_size/2)),trunc_size-3,int(trunc_size-(trunc_size/2))]
 
+#Connection Details
+hostname = "ntnx-10-53-87-119-a-pcvm"
+port = 22  # Default SSH port
+username = "nutanix"
+password = "nutanix/4u"
+
+# Commands to execute
+commands = [
+    "ssh 10.195.214.110",  # Get system information
+    "afs -S",     # Disk usage
+    "tiering.service_status verbose=true"     # System uptime
+]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 def get_md5sum(file_path):
 
     command = "md5sum {}".format(file_path)
@@ -29,18 +62,26 @@ for i in range(101):
     file1="sudo dd if=/home/nutanix/Text_500KB/File-5uK4c.txt  of=/local_test/local_file_${}-512k-64bs.txt bs=64K count=8 conv=notrunc".format(i)
     os.system(file1)
     time.sleep(1)
-    trunc_file = "/local_test/local_file_{}-512k-64bs.txt".format(i)
-    file_path = trunc_file
+    file1_create = "/local_test/local_file_{}-512k-64bs.txt".format(i)
+    file_path = file1_create
     output, split_output, error = get_md5sum(file_path)
-    trunc_file_md5 = str(split_output[0])
+    file1_md5 = str(split_output[0])
 
     file2 = "sudo dd if=/home/nutanix/Text_500KB/File-5uK4c.txt  of=/mnt/addr_node1_gps1/mount_point_test/local_file_$i-512k-64bs.txt bs=64K count=8 conv=notrunc".format(i)
     os.system(file2)
     time.sleep(1)
-    trunc_file = "/mnt/addr_node1_gps1/mount_point_test/local_file_{}-512k-64bs.txt".format(i)
-    file_path = trunc_file
+    file2_create = "/mnt/addr_node1_gps1/mount_point_test/local_file_{}-512k-64bs.txt".format(i)
+    file_path = file2_create
     mp_output, split_output, mp_error = get_md5sum(file_path)
-    mp_trunc_file_md5 = str(split_output[0])
+    mp_file_md5 = str(split_output[0])
+
+    if file1_md5 == mp_file_md5:
+        print("{}:{}  Md5sum of File creation at Local & MountPoint are MATCHING".format(file1_md5, mp_file_md5))
+    else:
+        print("The files: {} and {} , md5sum {}:{} check of File creation at Local & MountPoint are NOT MATCHING".format(file1_create, file2_create, file1_md5,mp_file_md5))
+        sys.exit()
+
+
 
 
 
